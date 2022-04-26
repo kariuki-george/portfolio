ab9c59651300735bee6561861a51d3ee90016837
smiley-geek <kariukigeorge2030@gmail.com>
2022-04-26 18:36:04 +0000

    New: new packages and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "420px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        black: {
+          500: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
