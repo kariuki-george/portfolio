6c4a65aad214d32312c2900145e86eb01e6a9699
smiley-geek <kariukigeorge2030@gmail.com>
2022-04-26 18:38:21 +0000

    Update: remove the unnecessary code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,12 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>smiley-geek</title>
-        <meta
-          name="description"
-          content="This is a portfolio for Kariuki George Thuo"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to smiley-geek hub</h1>
-
-        <p className={styles.description}>
-          If you see this page, the site is still under development. Thanks for
-          your visit.
-        </p>
-      </main>
+    <div>
+      
+      <Header/>
     </div>
   );
 };
