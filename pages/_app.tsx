681647f1b43dc62f2c613c3e05f82a4d1460d74c
smiley-geek <kariukigeorge2030@gmail.com>
2022-04-26 18:37:44 +0000

    Update: Add the new layout changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Global from "../layouts/Global.layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Global>
+      <Component {...pageProps} />
+    </Global>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body>
+        <body className="bg-black-500 ">
           <Main />
           <NextScript />
         </body>
