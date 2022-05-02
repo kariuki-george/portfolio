032757ac2f90b79a853c46ddbf70c0c810f23854
smiley-geek <kariukigeorge2030@gmail.com>
2022-05-02 10:41:39 +0000

    Update: swapped img for Image, added contacts functionality

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,18 @@
 import { NextPage } from "next";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Hero: NextPage = () => {
   return (
     <div className="flex w-full sm:flex-col min-h-screen md:flex-row items-center justify-center md:justify-start  relative  p-5 py-4">
-      <section className="sm:inline md:hidden m-3 my-10    w-full">
-        <img className="w-full " src="/heropage.svg" alt="" />
+      <section className="sm:inline md:hidden m-3 my-10  h-[200px]   w-full relative">
+        <Image
+          layout="fill"
+          objectFit="fill"
+          src="/heropage.svg"
+          alt="hero image"
+        />
       </section>
       <section className="w-full flex flex-col items-center  md:items-start lg:items-center md:w-1/2">
         <span className="text-5xl sm:text-4xl">
@@ -13,12 +20,19 @@ const Hero: NextPage = () => {
         </span>
 
         <p className="text-xl">Freelance fullstack web developer</p>
-        <button className="w-2/3   my-10 text-green-brand  h-10 rounded border-green-brand border hover:bg-green-brand/5 hover:text-white">
-          let&apos;s build something great
-        </button>
+        <Link href="#contacts" passHref>
+          <button className="w-2/3   my-10 text-green-brand  h-10 rounded border-green-brand border hover:bg-green-brand/5 hover:text-white">
+            let&apos;s build something great
+          </button>
+        </Link>
       </section>
-      <section className="sm:hidden md:flex w-2/3 absolute right-0   h-full justify-center ">
-        <img className="w-full mb-4  " src="/heropage.svg" alt="" />
+      <section className=" pb-4 sm:hidden md:flex w-2/3 absolute right-0   h-full justify-center ">
+        <Image
+          layout="fill"
+          objectFit="fill"
+          src="/heropage.svg"
+          alt="hero image"
+        />
       </section>
     </div>
   );
