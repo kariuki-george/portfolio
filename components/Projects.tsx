cd814f504f1b024773b9c0c5f20153c7512a690b
smiley-geek <kariukigeorge2030@gmail.com>
2022-05-02 10:43:22 +0000

    Pass

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,21 +1,29 @@
 import { NextPage } from "next";
+import Image from "next/image";
 import React from "react";
 
 const Projects: NextPage = () => {
   return (
-    <div className=" w-full p-5 flex flex-col items-center">
+    <div id="projects"  className=" w-full p-5 flex flex-col items-center">
       <section className="text-4xl my-5 underline  decoration-green-brand/40 decoration-wavy">
         previous work
       </section>
       <section className="w-full flex sm:flex-col md:flex-row md:flex-wrap items-center justify-center my-5">
         {projects.map((project) => (
-          <a href={project.link} target="_blank" rel="noreferrer" className="sm:w-2/3  md:w-[400px]" key={project.name}>
-            <li className="w-full decoration-none border rounded-md p-2 flex flex-col items-center">
+          <a
+            href={project.link}
+            target="_blank"
+            rel="noreferrer"
+            className="sm:w-2/3  md:w-[400px]"
+            key={project.name}
+          >
+            <li className=" w-full decoration-none border rounded-md p-2 flex flex-col items-center ">
               <img
                 className="w-full md:object-cover"
                 src={project.imgUrl}
                 alt=""
               />
+
               <span className="text-xl">{project.name}</span>
             </li>
           </a>
