2dda8c02d685fe8874fc84a788323bd0137faa27
smiley-geek <kariukigeorge2030@gmail.com>
2022-05-02 10:48:10 +0000

    Fix: removed index.getlayout..

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import About from "./About";
 import Skills from "./Skills";
 
-import White from "../../layouts/White";
 const index = () => {
   return (
     <div className="  bg-transpatent text-black-500   ">
@@ -16,9 +15,7 @@ const index = () => {
   );
 };
 
-index.getLayout = (page:typeof index)=>{
-  return <White>{page}</White>
-}
+
 export default index;
 
 
