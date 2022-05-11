6aaf95b874aecb41584c878f92c820e3e5c118d7
smiley-geek <kariukigeorge2030@gmail.com>
2022-05-11 08:38:10 +0000

    Update:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Global from "../layouts/Global";
-import {NextPage} from "next"
-import {ReactElement, ReactNode} from "react"
-
-
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "../lib/apollo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Global>
-      <Component {...pageProps} />
-    </Global>
+    <ApolloProvider client={client}>
+      <Global>
+        <Component {...pageProps} />
+      </Global>
+    </ApolloProvider>
   );
 }
 
