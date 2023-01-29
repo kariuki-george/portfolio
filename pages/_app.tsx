073ea334af6c9a3d1aa6acb79f9a6e84fe692cbf
smiley-geek <kariukigeorge2030@gmail.com>
2023-01-29 18:26:52 +0000

    ci: added a ackee analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,42 @@ import type { AppProps } from "next/app";
 import Global from "../layouts/Global";
 
 import CommandBar from "../components/kbar";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import useAckee from "use-ackee";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // tracking
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      useAckee(
+        url,
+        {
+          server: "https://ackee.p.kariukigeorge.me",
+          domainId: "bba72abc-f3b0-482d-b4ac-5efb2e60f17d",
+        },
+        {
+          detailed: false,
+          ignoreLocalhost: !true,
+          ignoreOwnVisits: true,
+        }
+      );
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-      <Global>
-        <CommandBar>
-          <Component {...pageProps} />
-        </CommandBar>
-      </Global>
+    <Global>
+      <CommandBar>
+        <Component {...pageProps} />
+      </CommandBar>
+    </Global>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://ackee.p.kariukigeorge.me/tracker.js"
+            data-ackee-server="https://ackee.p.kariukigeorge.me"
+            data-ackee-domain-id="bba72abc-f3b0-482d-b4ac-5efb2e60f17d"
+          ></script>
+        </Head>
         <body className="bg-black-500 max-w-screen overflow-x-hidden ">
           <Main />
           <NextScript />
